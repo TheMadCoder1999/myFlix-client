029b79b0c681831471bd60446d812905d8546791
TheMadCoder1999 <160189908+TheMadCoder1999@users.noreply.github.com>
2024-07-10 19:07:29 -0400

    removed the movie list an added code to have the app pull from the heroku app

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,35 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 
 export const MainView = () => {
-    const [movies, SetMovies] = useState([
-        {
-            id: 1,
-            Title: 'Alien',
-            Director: 'Ridley Scott',
-            Genre:'Horror',
-            ImageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Alien_movie_poster.jpg/220px-Alien_movie_poster.jpg'
-        },
-        {
-            id: 2,
-            Title: 'Aliens',
-            Director: 'James Cameron',
-            Genre: 'Action',
-            ImageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_poster.jpg'
-        },
-        {
-            id: 3,
-            Title: 'The Mummy',
-            Director: 'Stephen Sommers',
-            Genre: 'Adventure',
-            ImageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/68/The_mummy.jpg/220px-The_mummy.jpg'
-        },
-]);
+
+const [movies, setMovies] = useState([]);
+
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  useEffect(() => {
+    fetch("https://xaviermovieapi-7207728f28d4.herokuapp.com/")
+        .then((response) => response.json())
+        .then(movies => {
+            setMovies(movies)
+        })
+        .catch(e => console.log(e))
+
+}, []);
+      
   if (selectedMovie) {
     return (
       <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
