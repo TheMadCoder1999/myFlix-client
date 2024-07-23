b9c2658bebe4ba174402a66046a56abf726bea05
TheMadCoder1999 <160189908+TheMadCoder1999@users.noreply.github.com>
2024-07-23 17:43:47 -0400

    applied bootstrap elements to my signup-view and cleaned up the code in the login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -59,24 +59,28 @@ if (data.user) {
 return (
   <Form onSubmit={handleSubmit}>
     <Form.Group controlId="formUsername">
-      <Form.Label>Username:</Form.Label>
-      <Form.Control
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        required
-        minLength="3" 
-      />
+      <Form.Label>
+      Username:
+      </Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3" 
+        />
     </Form.Group>
 
     <Form.Group controlId="formPassword">
-      <Form.Label>Password:</Form.Label>
-      <Form.Control
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
+      <Form.Label>
+      Password:
+      </Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
     </Form.Group>
     <Button variant="primary" type="submit">
       Submit
