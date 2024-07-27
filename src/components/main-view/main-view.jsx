880eb7b5e145e92dd7f4a6d2aa9f25dda5e65e2b
TheMadCoder1999 <160189908+TheMadCoder1999@users.noreply.github.com>
2024-07-27 12:36:39 -0400

    imported react-router-dom to the main view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,7 +3,9 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import {SignupView} from "../signup-view/signup-view";
-import {Row, Col} from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
